aeb92fbe1bd875dd2791ed7fe325a40f9df2d0c7
Tschongsti <lukas.angster@gmail.com>
2025-05-22 13:03:00 +0200

    aspira.dart in main.dart überführt und gelöscht

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:aspira/aspira.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:aspira/screens/start_screen.dart';
+import 'package:aspira/screens/tabs.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color(0XFF8D6CCB),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color(0xFFBCA7E6),
+);
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,8 +22,53 @@ void main() {
     DeviceOrientation.portraitUp,
   ]).then((fn) {
   runApp(
-    const Aspira(),
+    const ProviderScope(
+      child: AspiraApp(),
+    ),
   );
   });
 }
 
+class AspiraApp extends StatefulWidget {
+  const AspiraApp({super.key});
+
+  @override
+  State<AspiraApp> createState() {
+    return _AspiraAppState();
+  }
+}
+
+class _AspiraAppState extends State<AspiraApp> {
+  var activeScreen = 'start-screen';
+
+  void startApp() {
+    setState(() {
+      activeScreen = 'tabs-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(startApp);
+
+    if (activeScreen == 'tabs-screen') {
+      screenWidget = const TabsScreen();
+    }
+
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        textTheme: GoogleFonts.interTextTheme(),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+      ),
+      home: screenWidget,
+    );
+  }
+}
